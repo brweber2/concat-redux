70d6ac6fa5ca149d11f82e8700c9867ab8579fb2
Bryan Weber <brweber2@yahoo.com>
2012-02-19 23:55:32 -0500

    adding unit test (still not passing) for getting my initial example to work. time to add strings and number literals from the lexer on through...

---
+++ src/test/java/com/brweber2/ReadFileAndRunItTest.java
@@ -0,0 +1,29 @@
+package com.brweber2;
+
+import com.brweber2.call.PrintlnCall;
+import com.brweber2.vocab.Vocabulary;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author brweber2
+ *         Copyright: 2012
+ */
+public class ReadFileAndRunItTest {
+    
+    @Test
+    public void readFileAndRunIt() throws IOException {
+        Vocabulary.getCurrent().register("println", new PrintlnCall() );
+        String source = "\n\n:define wow ( java.lang.String ->  ) { println . } .\n" +
+                        ":wow \"Hello\" .";
+        File file = File.createTempFile("readFileAndRunIt", UUID.randomUUID().toString());
+        FileWriter writer = new FileWriter(file);
+        writer.write(source);
+        writer.close();
+        Runtime.main(new String[] {file.getAbsolutePath()});
+    }
+}
